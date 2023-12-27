85c744739e9b001678bda8e35e0bc0186df53746
mustyAY <mustyyahya.1580@yahoo.com>
2023-12-28 00:36:33 +0100

    [id] page responsiveness with sidebar

--- pages/[id].jsx
+++ pages/[id].jsx
@@ -39,7 +39,7 @@ export default function commentsPage({ tweetData }) {
         <div>
             <div className='min-h-screen max-w-[1400px] mx-auto flex'>
                 <Sidebar />
-                <div className="sm:ml-16 xl:ml-[400px] max-w-[550px] flex-grow border-x border-gray-200">
+                <div className="ml-16 xl:ml-[400px] w-[100%] max-w-[550px] flex-grow border-x border-gray-200">
                     <div className="flex space-x-3 px-3 py-2 text-lg sm:text-xl font-bold 
                     border-b border-gray-200 sticky top-0 z-50 bg-white">
                         <Link href={"/"}>
