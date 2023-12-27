aac3fb655c172a67a05f521b3336d24cc6cbcf62
mustyAY <mustyyahya.1580@yahoo.com>
2023-12-28 00:26:02 +0100

    posts feed width responsiveness

--- components/PostsFeed.jsx
+++ components/PostsFeed.jsx
@@ -17,7 +17,7 @@ export default function PostsFeed() {
     }, []);
 
     return (
-        <div className="ml-16 w-[50%] xl:ml-[400px] max-w-[550px] flex-grow border-x border-gray-200">
+        <div className="ml-16 w-[100%] xl:ml-[400px] max-w-[550px] flex-grow border-x border-gray-200">
             <div className="px-3 py-2 text-lg sm:text-xl font-bold border-b border-gray-200 sticky top-0
             z-50 bg-white">
                 Home
